e5ac428d730e76c7cee41f611fcafc356104ef0e
Aahh-shesshh <122276824+Aahh-shesshh@users.noreply.github.com>
2023-03-16 09:16:59 +0545

    commit

--- src/App.js
+++ src/App.js
@@ -7,20 +7,10 @@ import Body from "./components/Body";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
-  // const [index, setIndex] = React.useState(0);
-  // const [buttonText, setButtonText] = React.useState("Start Quiz");
-  // const navigate = useNavigate();
 
-  
-
-  //   const navigateToBody = () => {
-  //     // 👇️ navigate to /contacts
-  //     navigate('/Body');
-  //   };
-  
 
   return (
-    <BrowserRouter>
+    <BrowserRouter  basename="">
       <Routes> 
         <Route path="/" element={<Intro />} />   
         <Route path="/Body" element={ <Body />} />
